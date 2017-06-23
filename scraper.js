b9d48f7a956f808937fd9fa85894ef5c7dd1a8db
David Ferguson <git@dferguson.com>
2017-06-22 23:25:19 -0700

    Latest

--- scraper.js
+++ scraper.js
@@ -117,16 +117,13 @@ request(url, function (error, response, body) {
                                           }
                                       };
 
-
+                                      console.log(optionsMyNumbersPut);
 
                                       function callbackMyNumbersPut(error, response, body) {
                                         if (!error && response.statusCode == 200) {
                                           var info = body;
                                           console.log(info);
                                           }
-                                         else {
-                                             console.log("error");
-                                         }
                                       }
                                       request(optionsMyNumbersPut, callbackMyNumbersPut);
                                   }
